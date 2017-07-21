b5e4c137c6f28069d04937fb1102a73b098c0148
Naril <narilgvb@gmail.com>
2017-07-21 03:55:11 +0200

    Player moves and camera follow,

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    private Transform player;
+    public float smothTime;
+    private float velocity;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        float x = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity, smothTime);
+
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Animator anim;
+    public float speed = 6f;
+    Vector2 movement;
+    Rigidbody2D playerRigidbody;
+
+	// Use this for initialization
+	void Awake () {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        movement.Set(Input.GetAxisRaw("Horizontal"),0f);
+        Move();
+	}
+
+    void Move()
+    {
+        Vector3 newScale = transform.localScale;
+
+        if(movement.x != 0f)
+        {
+            float sign = Mathf.Sign(Input.GetAxis("Horizontal"));
+            newScale.x = sign * Mathf.Abs(transform.localScale.x);
+        }
+
+        transform.localScale = newScale;
+
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        playerRigidbody.MovePosition(transform.position + new Vector3(movement.x, movement.y, 0f));
+    }
+}
