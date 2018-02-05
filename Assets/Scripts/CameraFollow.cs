a55b24706184971cccad60e13a7a9243f2fdc86f
Naril <narilgvb@gmail.com>
2018-02-05 21:14:03 +0100

    Camera only moves when player cross a limit.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,17 +6,65 @@ public class CameraFollow : MonoBehaviour {
 
     private Transform player;
     public float smothTime;
+    public float smothTimeWall;
     private float velocity;
+    public float maxWall;
+    public float minWall;
+    public float maxMove;
+    public float minMove;
+
+    public Transform Firstwall;
+    public Transform Lastwall;
+
+    public Transform MinMoveWall;
+    public Transform MaxMoveWall;
+
+    bool move = false;
 
     void Start()
     {
+
+        minWall = Firstwall.position.x;
+        maxWall = Lastwall.position.x;
+
         player = GameObject.Find("Player").transform;
     }
 
-    void Update()
+    void FixedUpdate()
     {
-        float x = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity, smothTime);
+        minMove = MinMoveWall.position.x;
+        maxMove = MaxMoveWall.position.x;
 
-        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+        if (player.position.x < minMove || player.position.x > maxMove)
+        {
+            move = true;
+        }
+        else if(player.position.x > transform.position.x - 0.2f && player.position.x < transform.position.x + 0.2f)
+        {
+            move = false;
+        }
+
+        if (player.position.x > minWall && player.position.x < maxWall) {
+            if (move)
+            {
+                float x = Mathf.SmoothDamp(transform.position.x, player.position.x, ref velocity, smothTime);
+
+                GetComponent<Rigidbody2D>().MovePosition(new Vector3(x, transform.position.y, transform.position.z));
+            }
+        }
+        else
+        {
+            if (player.position.x <= minWall) {
+                float x = Mathf.SmoothDamp(transform.position.x, Firstwall.position.x, ref velocity, smothTimeWall);
+
+                GetComponent<Rigidbody2D>().MovePosition(new Vector3(x, transform.position.y, transform.position.z));
+            }
+            else if (player.position.x >= maxWall)
+            {
+                float x = Mathf.SmoothDamp(transform.position.x, Lastwall.position.x, ref velocity, smothTimeWall);
+
+                GetComponent<Rigidbody2D>().MovePosition(new Vector3(x, transform.position.y, transform.position.z));
+            }
+        }
     }
 }
